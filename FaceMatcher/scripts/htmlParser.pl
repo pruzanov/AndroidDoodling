1f19e8a62f3df62c02a71561c9edd5b5f622dbf8
Peter Ruzanov <pruzanov@gmail.com>
2016-05-06 12:13:16 -0400

    Updated htmlParser.pl script

--- FaceMatcher/scripts/htmlParser.pl
+++ FaceMatcher/scripts/htmlParser.pl
@@ -2,15 +2,29 @@
 
 #
 # Simple html parser for FaceMatcher
+# will read from the list of files and parse them, optionally checking if we have existing image
+# for an entry
 #
 
 use strict;
 use Data::Dumper;
+use Getopt::Long;
+use File::Basename;
 use IO::File;
 use JSON;
 use constant DEBUG=>0;
 
-my @files = ();
+
+my $USAGE = "htmlParser.pl --list [list of html files] --img-dir [Optional, dir with existing images]";
+my($list,$imgdir);
+my $result = GetOptions("list=s"         => \$list,
+                        "img-dir=s"      => \$imgdir);
+
+unless($list) { die $USAGE; }
+open(LIST,"<$list") or die "Couldn't read from the list [$list]";
+my @files = map{chomp; $_;} (<LIST>);
+close LIST;
+
 while (my $in = shift @ARGV) {
  push (@files, $in);
 }
@@ -21,7 +35,6 @@ my $results = {people=>[]};
 my %seen = ();
 map{&add_to_results($_)} @files;
 
-#print Dumper($results) if DEBUG;
 print STDERR scalar(@{$results->{people}})." items retrieved\n";
 my $json = JSON->new();
 $json = encode_json $results;
@@ -46,16 +59,22 @@ sub add_to_results {
  }
  $fh->close();
 
-  while($html_text=~/Items\d*_files\/(.\w+?.jpg)".+?="ms-cellstyle ms-vb2\">(.+?)<\/td/gc) {
-       my $image = $1;
+  while($html_text=~/Items\d*_files\/(.\w+?.jpg)".+?="ms-cellstyle ms-vb2\">(.+?)<\/td/igc) {
+       my $image = basename($1);
        my $name  = $2;
+       ($image,$name) = ($1,$2) if $name=~m!/(\S+?.jpg)\"\s+alt=\"(.+?)\"!i;
        if ($image =~/^blank\./){next;}
        $image =~s/\-/_/g;
-       $image =~s!.*/!!;
        $image =~tr/A-Z/a-z/;
        $image =~s/jpg/png/;
        $name =~s/&#39;/'/g;
+       # Skip Names without image (optional)
+       if ($imgdir) {
+         my $img_path = join("/",($imgdir,$image));
+         unless (-e $img_path) { print STDERR "Skipping $name\t(no image available)\n";next; }
+       }
        if (!$seen{$name}++) {
+        print STDERR "Name: $name\n" if DEBUG;
         push(@{$results->{people}}, {name=>$name, png=>$image});
         $count++;
        }
